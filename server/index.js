318ed9837f5f0b1302408d0fe21c353e0173eb81
Igor333m <igor333m@yahoo.com>
2019-10-22 13:03:01 -0700

    Initial setup for google OAuth 2.0 with passport.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require('express'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth20').Strategy,
+    keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (requst, response) => {
-    response.send({hello: 'world'});
-});
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log("index / accessToken: ", accessToken);
+    })
+); 
 
 // listen for Heroku environment variable PORT
 const PORT = process.env.PORT || 5000;
