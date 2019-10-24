98042de440a0e359748474419f021b0b3ee94477
Igor333m <igor333m@yahoo.com>
2019-10-24 09:34:41 -0700

    Get user profile data

--- server/index.js
+++ server/index.js
@@ -9,14 +9,19 @@ passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
+    }, (accessToken, refreshToken, profile, done) => {
         console.log("index / accessToken: ", accessToken);
+        console.log("index / refreshToken: ", refreshToken);
+        console.log("index / profile: ", profile);
+        console.log("index / done: ", done);
     })
 );
 
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // listen for Heroku environment variable PORT
 const PORT = process.env.PORT || 5000;
